4523d942c66afbc514063f4c8332082740f3f005
lamtanng <laam122003@gmail.com>
2023-10-09 10:16:07 +0700

    lâm commit

--- Ielts-listening2/src/main/java/hcmute/DAO/DBConnection.java
+++ Ielts-listening2/src/main/java/hcmute/DAO/DBConnection.java
@@ -19,6 +19,7 @@ public class DBConnection {
 	
 	public static Connection getConnection() throws IOException{
 		con = null ;
+		//nguyen tan lam
 		try {
 			//driver register-- test 
 			DriverManager.registerDriver(new Driver());
