b39328a6be32da83ea3cc8e2315300cc2c57c2d8
Nguyễn Quang Vinh VT <nguyenquangvinhwork1207@gmail.com>
2023-10-24 14:58:32 +0700

    them thieu

--- Ielts-listening2/src/main/java/hcmute/controllers/TestControllers.java
+++ Ielts-listening2/src/main/java/hcmute/controllers/TestControllers.java
@@ -8,7 +8,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-@WebServlet(urlPatterns = { "/kiemtra/luyende_test"})
+@WebServlet(urlPatterns = { "/test/luyende_test"})
 public class TestControllers extends HttpServlet{
 
 	@Override
