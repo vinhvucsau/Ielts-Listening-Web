1f44bec07f8df048ccdfa0e144e1b289a82085b6
quochuywork <huypro0320092003@gmail.com>
2023-10-26 19:25:07 +0700

    fix loi enrrolmentDate

--- Ielts-listening2/src/main/java/hcmute/DAO/Course/CourseDAOImpl.java
+++ Ielts-listening2/src/main/java/hcmute/DAO/Course/CourseDAOImpl.java
@@ -7,17 +7,15 @@ import javax.persistence.TypedQuery;
 
 import JPAConfig.JPAConfig;
 import hcmute.entity.Course;
-import hcmute.utils.Constants;
 
-public class CourseDAOImpl implements ICourseDAO{
+public class CourseDAOImpl implements ICourseDAO {
 
 	@Override
 	public List<Course> getAllCourses() {
-		EntityManager enma = JPAConfig.getEntityManager();
-		String name = Constants.COURSE_RELATION;
-		String jpql = "SELECT c FROM COURSE c";
-		TypedQuery<Course> q = enma.createQuery("SELECT c FROM COURSE c", Course.class);
-		return q.getResultList();
+		EntityManager entityManager = JPAConfig.getEntityManager();
+		String jpql = "SELECT c FROM Course c"; 
+		TypedQuery<Course> query = entityManager.createQuery(jpql, Course.class);
+		return query.getResultList();
 	}
 
 }
