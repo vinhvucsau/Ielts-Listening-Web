715ab90bad5ffaf5a07fcdf8f68f2b9be89a5361
Lương Bùi <luonglb1311@gmail.com>
2023-12-05 10:30:45 +0700

    Cập nhật BE trang Profile
    - User Info:
       + Kiểm tra độ dài và định dạng sđt
       + Kiểm tra trùng sđt/email
    - Security:
       + Kiểm tra mật khẩu cũ
       + Kiểm tra xác nhận mật khẩu mới

--- Ielts-listening2/src/main/java/hcmute/services/IUserService.java
+++ Ielts-listening2/src/main/java/hcmute/services/IUserService.java
@@ -5,4 +5,6 @@ import hcmute.entity.User;
 public interface IUserService {
 	User findUserByID(String id);
 	void update (User user);
+	boolean findDuplicateEmail(String email, String userId);
+	boolean findDuplicatePhone (String phoneNum, String userId);
 }
