f014681538862f2d9eccfe669746810b3d5fd22f
Wayy8T <waytnt17523@gmail.com>
2023-12-09 01:21:39 +0700

    FE và BE trang detail

--- Ielts-listening2/src/main/java/hcmute/DAO/IPayDetailDAO.java
+++ Ielts-listening2/src/main/java/hcmute/DAO/IPayDetailDAO.java
@@ -1,5 +1,12 @@
 package hcmute.DAO;
 
+import java.util.List;
+
+import hcmute.entity.PayDetail;
+
 public interface IPayDetailDAO {
+
+	List<PayDetail> findPayDetailByIDPayment(String idpay);
+
 
 }
