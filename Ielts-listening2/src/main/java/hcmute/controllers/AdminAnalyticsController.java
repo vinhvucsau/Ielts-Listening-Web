6cd60b37aa511af34be5e6a78bc95adf538b8160
Lương Bùi <luonglb1311@gmail.com>
2023-12-09 16:03:18 +0700

    Cập nhật FE trang Thống kê (Admin)

---
+++ Ielts-listening2/src/main/java/hcmute/controllers/AdminAnalyticsController.java
@@ -0,0 +1,31 @@
+package hcmute.controllers;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet (urlPatterns = {"/admin/analytics"})
+public class AdminAnalyticsController extends HttpServlet{
+
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		String url = req.getRequestURI().toString();
+		if (url.contains("analytics")) {
+			RequestDispatcher rd = req.getRequestDispatcher("/views/admin/Analytics.jsp");
+			rd.forward(req, resp);
+		}
+	}
+	
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		super.doPost(req, resp);
+	}
+}
