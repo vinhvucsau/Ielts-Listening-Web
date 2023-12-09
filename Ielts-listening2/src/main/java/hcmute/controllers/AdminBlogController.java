fe473375929a7c1c16036438d83b1db04170ee3b
lamtanng <laam122003@gmail.com>
2023-12-10 00:18:59 +0700

    BE chức năng blog User/Admin
    (Chưa hiển thị chi tiết)

--- Ielts-listening2/src/main/java/hcmute/controllers/AdminBlogController.java
+++ Ielts-listening2/src/main/java/hcmute/controllers/AdminBlogController.java
@@ -1,6 +1,7 @@
 package hcmute.controllers;
 
 import java.io.IOException;
+import java.util.Date;
 import java.util.List;
 
 import javax.servlet.RequestDispatcher;
@@ -19,9 +20,11 @@ import hcmute.services.IBlogService;
 import hcmute.services.IUserService;
 import hcmute.services.UserServiceImpl;
 import hcmute.utils.Constants;
+import hcmute.utils.DeleteImage;
+import hcmute.utils.UploadUtils;
 
 @MultipartConfig
-@WebServlet(urlPatterns = {"/admin/blogs", "/admin/edit-blog", "/admin/update-blog-status"})
+@WebServlet(urlPatterns = {"/admin/blogs", "/admin/edit-blog", "/admin/add-blog", "/admin/update-blog-status", "/admin/blog-content"})
 public class AdminBlogController extends HttpServlet {
 
 	private static final long serialVersionUID = 1L;
@@ -30,6 +33,7 @@ public class AdminBlogController extends HttpServlet {
 	IUserService uService = new UserServiceImpl();
 	
 	User user = new User();//session login
+	Date currentDay = new Date(); //curren day
 	
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
@@ -45,8 +49,9 @@ public class AdminBlogController extends HttpServlet {
 		if (session != null && session.getAttribute("user") != null) {
 			user = (User) session.getAttribute("user");
 		} else {
-//			RequestDispatcher rd = req.getRequestDispatcher("/views/user/error404.jsp");
-//			rd.forward(req, resp);
+			req.setAttribute("e", "Chưa đăng nhập !");
+			RequestDispatcher rd = req.getRequestDispatcher("/views/user/error404.jsp");
+			rd.forward(req, resp);
 		}
 		
 		if (url.contains("blogs")) {
@@ -66,6 +71,95 @@ public class AdminBlogController extends HttpServlet {
 				RequestDispatcher rd = req.getRequestDispatcher("/views/user/error404.jsp");
 				rd.forward(req, resp);
 			}
+		} else if (url.contains("add-blog")) {
+			RequestDispatcher rd = req.getRequestDispatcher("/views/user/addBlog.jsp");
+			rd.forward(req, resp);
+		} else if (url.contains("edit-blog")) {
+			String id = req.getParameter("id");
+			Blog oldBlog = blogService.findOneById(id);
+
+			req.setAttribute("blog", oldBlog);
+			RequestDispatcher rd = req.getRequestDispatcher("/views/user/editBlog.jsp");
+			rd.forward(req, resp);
+		} else if (url.contains("blog-content")) {
+			
+			req.setAttribute("id", req.getParameter("id"));
+			RequestDispatcher rd = req.getRequestDispatcher("/views/user/blog_content.jsp");
+			rd.forward(req, resp);
+		}
+	}
+	
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		String url = req.getRequestURI().toString();
+		req.setCharacterEncoding("UTF-8");
+		resp.setCharacterEncoding("UTF-8");
+		HttpSession session = req.getSession();
+		
+		if (url.contains("add-blog")) {
+			if (req.getPart("image").getSize() != 0) {
+				Blog newBlog = new Blog();
+				long millis = System.currentTimeMillis();
+				Date date = new Date(millis);
+				newBlog.setCreatedDate(date);
+				String fileName = "" + System.currentTimeMillis();
+				newBlog.setImage((UploadUtils.processUpload("image", req, Constants.DIR + "\\"+ Constants.FOLDER_BLOG+"\\", fileName)));
+
+				String title = req.getParameter("title");
+				String content = req.getParameter("content");
+				User user = (User) session.getAttribute("user");
+				
+				newBlog.setBlogId("ID");
+				newBlog.setContent(content);
+				newBlog.setStatus(0);
+				newBlog.setTitle(title);
+				newBlog.setUsers(user);
+
+				try {
+					blogService.insert(newBlog);
+					resp.sendRedirect(req.getContextPath() + "/admin/blogs");
+					
+				} catch (Exception e) {
+					req.setAttribute("e", e.getMessage());
+					RequestDispatcher rd = req.getRequestDispatcher("/views/user/error404.jsp");
+					rd.forward(req, resp);
+				}
+			}
+		} else if (url.contains("edit-blog")) {
+			String id = req.getParameter("id");
+			String title = req.getParameter("title");
+			String content = req.getParameter("content");
+			
+			Blog newBlog = new Blog();
+			Blog oldBlog = blogService.findOneById(id);
+
+			newBlog.setTitle(title);
+			newBlog.setContent(content);
+			newBlog.setBlogId(id);
+			newBlog.setCreatedDate(currentDay);
+			newBlog.setStatus(oldBlog.getStatus());
+			newBlog.setUsers(user);
+			
+			try {
+				if (req.getPart("image").getSize() == 0) {
+					newBlog.setImage(oldBlog.getImage());
+				} else {//xoa anh cu
+					if (oldBlog.getImage() != null) {
+						DeleteImage.deleteImage(oldBlog.getImage(), Constants.FOLDER_BLOG);
+					} // update anh moi
+					String fileName = "" + System.currentTimeMillis();
+					newBlog.setImage((UploadUtils.processUpload("image", req, Constants.DIR + "\\"+Constants.FOLDER_BLOG+"\\", fileName)));
+				}
+
+				blogService.update(newBlog);
+				resp.sendRedirect(req.getContextPath() + "/admin/blogs");
+			} catch (Exception e) {
+				req.setAttribute("e", e.getMessage());
+				RequestDispatcher rd = req.getRequestDispatcher("/views/user/error404.jsp");
+				rd.forward(req, resp);
+			}
+			
+
 		}
 	}
 
