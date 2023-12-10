3332e26b52ba8d58c6d99430c9d7097c2ebefb65
quochuywork <huypro0320092003@gmail.com>
2023-12-10 14:15:53 +0700

    done admin-analys

---
+++ Ielts-listening2/src/main/java/hcmute/services/IAdminAnalysService.java
@@ -0,0 +1,32 @@
+package hcmute.services;
+
+import java.util.List;
+
+import hcmute.entity.Course;
+import hcmute.entity.User;
+
+public interface IAdminAnalysService {
+	Long countUser();
+	Long countCourse();
+	Long sumRevenue();
+	Long costWithMonth1();
+	Long costWithMonth2();
+	Long costWithMonth3();
+	Long costWithMonth4();
+	Long costWithMonth5();
+	Long costWithMonth6();
+	Long costWithMonth7();
+	Long costWithMonth8();
+	Long costWithMonth9();
+	Long costWithMonth10();
+	Long costWithMonth11();
+	Long costWithMonth12();
+	List<String> userIdDescreaseCost();
+	List<Long> costDecrease();
+	Long countCourseByUserId(String userId);
+	Long countRatingByUserId(String userId);
+	User findUserByUserId(String userId);
+	Long sumCostByUserId(String userId);
+	List<Course> listCourseOrderByTime();
+
+}
