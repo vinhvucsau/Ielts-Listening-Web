5cfcfe6bec915d46e1a988f4a24860779c8f7784
TranDuongThieu <thieu098dm3@gmail.com>
2023-12-11 11:53:10 +0700

    merge minh

--- Ielts-listening2/src/main/java/hcmute/controllers/AdminKhoaHocController.java
+++ Ielts-listening2/src/main/java/hcmute/controllers/AdminKhoaHocController.java
@@ -15,7 +15,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-
 import hcmute.entity.Course;
 import hcmute.entity.TopicTest;
 import hcmute.services.AdminKhoaHocServiceImpl;
@@ -26,12 +25,14 @@ import hcmute.utils.Constants;
 import hcmute.utils.UploadUtils;
 
 @MultipartConfig
-@WebServlet(urlPatterns = { "/admin/khoahoc", "/admin/deletecourse","/admin/insertCourse", "/admin/lesson","/admin/updateCourse" })
+@WebServlet(urlPatterns = { "/admin/khoahoc", "/admin/deletecourse", "/admin/insertCourse", "/admin/lesson",
+		"/admin/updateCourse" })
 public class AdminKhoaHocController extends HttpServlet {
 
 	private static final long serialVersionUID = 1L;
 	ICourseService courseService = new CourseServiceImpl();
 	IAdminKhoaHocService adminKhoaHocService = new AdminKhoaHocServiceImpl();
+
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
@@ -40,51 +41,51 @@ public class AdminKhoaHocController extends HttpServlet {
 		String rate = req.getParameter("rate") == null ? "" : req.getParameter("rate");
 		String searchStr = req.getParameter("search") == null ? "" : req.getParameter("search");
 		int page = Integer.parseInt(req.getParameter("page") == null ? "1" : req.getParameter("page"));
-		int tab=1;
+		int tab = 1;
 		int pagesize = 8;
 		if (url.contains("/deletecourse")) {
 			Delete(req, resp);
 			resp.sendRedirect(req.getContextPath() + "/admin/khoahoc");
-		}
-		else { if (gia.equals("thapdencao")) {
-			tab = 4;
-		} else if (gia.equals("caodenthap")) {
-			tab = 5;
-		} else if (rate.equals("thapdencao")) {
-			tab = 2;
-		} else if (rate.equals("caodenthap")) {
-			tab = 3;
-			
-		} else if (url.contains("lesson")) {
-			RequestDispatcher rd = req.getRequestDispatcher("/views/khoahoc/AdminLesson.jsp");
+		} else {
+			if (gia.equals("thapdencao")) {
+				tab = 4;
+			} else if (gia.equals("caodenthap")) {
+				tab = 5;
+			} else if (rate.equals("thapdencao")) {
+				tab = 2;
+			} else if (rate.equals("caodenthap")) {
+				tab = 3;
+
+			} else if (url.contains("lesson")) {
+				RequestDispatcher rd = req.getRequestDispatcher("/views/khoahoc/AdminLesson.jsp");
+				rd.forward(req, resp);
+			}
+			Long count = adminKhoaHocService.countKhoaHoc();
+			List<Course> allCourseList = adminKhoaHocService.findAll(searchStr, tab);
+			for (Course course : allCourseList) {
+				System.out.print("course" + course.getCourseId());
+			}
+			List<Course> CourseList = adminKhoaHocService.findAll(page - 1, pagesize, searchStr, tab);
+			req.setAttribute("countCourse", count);
+			int pageNum = (int) (allCourseList.size() / pagesize) + (allCourseList.size() % pagesize == 0 ? 0 : 1);
+			req.setAttribute("course", CourseList);
+			req.setAttribute("pagesize", pagesize);
+			req.setAttribute("pageNum", pageNum);
+			RequestDispatcher rd = req.getRequestDispatcher("/views/admin/AdminKhoaHoc.jsp");
 			rd.forward(req, resp);
 		}
-		Long count = adminKhoaHocService.countKhoaHoc();
-		List<Course> allCourseList = adminKhoaHocService.findAll(searchStr, tab);
-		for (Course course : allCourseList) {
-			System.out.print("course" + course.getCourseId());
-		}
-		List<Course> CourseList = adminKhoaHocService.findAll(page - 1, pagesize, searchStr, tab);
-		req.setAttribute("countCourse", count);
-		int pageNum = (int) (allCourseList.size() / pagesize) + (allCourseList.size() % pagesize == 0 ? 0 : 1);
-		req.setAttribute("course", CourseList);
-		req.setAttribute("pagesize", pagesize);
-		req.setAttribute("pageNum", pageNum);
-		RequestDispatcher rd = req.getRequestDispatcher("/views/admin/AdminKhoaHoc.jsp");
-		rd.forward(req, resp);
-		}
-		
+
 	}
+
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		String url = req.getRequestURI().toString();
 		req.setCharacterEncoding("UTF-8");
 		resp.setCharacterEncoding("UTF-8");
-		if (url.contains("insertCourse"))
-		{
+		if (url.contains("insertCourse")) {
 			Course model = new Course();
 			try {
-				//BeanUtils.populate(model, req.getParameterMap());
+				// BeanUtils.populate(model, req.getParameterMap());
 				model.setCourseId("");
 				model.setCourseName(req.getParameter("courseName"));
 				model.setDescription(req.getParameter("description"));
@@ -97,9 +98,10 @@ public class AdminKhoaHocController extends HttpServlet {
 				if (req.getPart("trailer").getSize() != 0) {
 					// tạo tên file mới để khỏi bị trùng
 					String fileName = "" + System.currentTimeMillis();
-					model.setTrailer(UploadUtils.processUpload("trailer", req, Constants.DIR + "\\courseTrailer\\", fileName));
+					model.setTrailer(
+							UploadUtils.processUpload("trailer", req, Constants.DIR + "\\courseTrailer\\", fileName));
 				}
-				Date date1 = new SimpleDateFormat("yyyy-MM-dd").parse(req.getParameter("enrrollmentDate")); 
+				Date date1 = new SimpleDateFormat("yyyy-MM-dd").parse(req.getParameter("enrrollmentDate"));
 				model.setEnrrolmentDate(date1);
 				courseService.insert(model);
 				req.setAttribute("course", model);
@@ -111,47 +113,47 @@ public class AdminKhoaHocController extends HttpServlet {
 			if (req.getPart("trailer").getSize() != 0) {
 				// tạo tên file mới để khỏi bị trùng
 				String fileName = "" + System.currentTimeMillis();
-				model.setTrailer(UploadUtils.processUpload("trailer", req, Constants.DIR + "\\"+Constants.FOLDER_VIDEO+"\\", fileName));
-			}
-		else if (url.contains("updateCourse"))
-			{
-			Course model = new Course();
-			Course course = courseService.findById(req.getParameter("CourseId"));
-			try {
-				//BeanUtils.populate(model, req.getParameterMap());
-				model.setCourseId(req.getParameter("CourseId"));
-				model.setCourseName(req.getParameter("CourseName"));
-				model.setDescription(req.getParameter("Description"));
-				model.setCost(Integer.parseInt(req.getParameter("Cost")));
-				if (req.getPart("Image").getSize() != 0) {
+				model.setTrailer(UploadUtils.processUpload("trailer", req,
+						Constants.DIR + "\\" + Constants.FOLDER_VIDEO + "\\", fileName));
+			} else if (url.contains("updateCourse")) {
+				model = new Course();
+				Course course = courseService.findById(req.getParameter("CourseId"));
+				try {
+					// BeanUtils.populate(model, req.getParameterMap());
+					model.setCourseId(req.getParameter("CourseId"));
+					model.setCourseName(req.getParameter("CourseName"));
+					model.setDescription(req.getParameter("Description"));
+					model.setCost(Integer.parseInt(req.getParameter("Cost")));
+					if (req.getPart("Image").getSize() != 0) {
 						// tạo tên file mới để khỏi bị trùng
-					String fileName = "" + System.currentTimeMillis();
-					model.setImage(UploadUtils.processUpload("Image", req, Constants.DIR + "\\courseIMG\\", fileName));
-				}
-				else {
-					model.setImage(course.getImage());
-				}
-				if (req.getPart("Trailer").getSize() != 0) {
+						String fileName = "" + System.currentTimeMillis();
+						model.setImage(
+								UploadUtils.processUpload("Image", req, Constants.DIR + "\\courseIMG\\", fileName));
+					} else {
+						model.setImage(course.getImage());
+					}
+					if (req.getPart("Trailer").getSize() != 0) {
 						// tạo tên file mới để khỏi bị trùng
-					String fileName = "" + System.currentTimeMillis();
-					model.setTrailer(UploadUtils.processUpload("Trailer", req, Constants.DIR + "\\courseTrailer\\", fileName));
+						String fileName = "" + System.currentTimeMillis();
+						model.setTrailer(UploadUtils.processUpload("Trailer", req, Constants.DIR + "\\courseTrailer\\",
+								fileName));
+					} else {
+						model.setTrailer(course.getTrailer());
+					}
+
+					Date date1 = new SimpleDateFormat("yyyy-MM-dd").parse(req.getParameter("EnrrollmentDate"));
+					model.setEnrrolmentDate(date1);
+					courseService.update(model);
+					req.setAttribute("course", model);
+					req.setAttribute("message", "add succes");
+				} catch (Exception e) {
+					e.printStackTrace();
+					req.setAttribute("error", "add faild");
 				}
-				else {
-					model.setTrailer(course.getTrailer());
-				}
-				
-				Date date1 = new SimpleDateFormat("yyyy-MM-dd").parse(req.getParameter("EnrrollmentDate")); 
-				model.setEnrrolmentDate(date1);
-				courseService.update(model);
-				req.setAttribute("course", model);
-				req.setAttribute("message", "add succes");
-			} catch (Exception e) {
-				e.printStackTrace();
-				req.setAttribute("error", "add faild");
+				resp.sendRedirect(req.getContextPath() + "/admin/khoahoc");
+
 			}
-			resp.sendRedirect(req.getContextPath()+"/admin/khoahoc");	
-	
-			}
+		}
 
 	}
 
